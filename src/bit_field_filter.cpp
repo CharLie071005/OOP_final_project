7e0e3b2e8d8e550961b30f41bcf32bdc85ddadb8
Ouzheee <jason36026@gmail.com>
2024-06-13 13:39:08 -0400

    Ou test

--- src/bit_field_filter.cpp
+++ src/bit_field_filter.cpp
@@ -3,7 +3,5 @@
 void Gray_Box_Filter(Image image){
     int *_h = &image.get_height;
     int *_w = &image.get_width;
-    int **_pixels = image.get_pixels;
-    
-    
+    int **_pixels = image.get_pixels;    
 }
