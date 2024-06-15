f11309924a41e944f45027a001c87682adbc0959
Ouzheee <jason36026@gmail.com>
2024-06-15 13:34:40 -0400

    ignore Final

--- main.cpp
+++ main.cpp
@@ -4,86 +4,81 @@
 #include "photo_mosaic.h"
 #include "bit_field_filter.h"
 
-#define GRAY_BOX    0b00000001
-#define GRAY_Med    0b00000010
-#define GRAY_Sobel  0b00000100
-#define GRAY_Linear 0b00001000
-#define RGB_BOX     0b00010000
-#define RGB_Med     0b00100000
-#define RGB_Sobel   0b01000000
-#define RGB_Linear  0b10000000
+#define option1 0b00001100
+#define option2 0b10100000
 #define ENABLE_X_SERVER false
 
+void TestGrayImage();
+void TestRGBImage();
+void TestPhotoMosaic();
 
+int main(int argc, char *argv[]) {
+    // TestGrayImage();
+    // TestRGBImage();
+    TestPhotoMosaic();
+    return 0;
+}
 
-int main(int argc, char *argv[]){
-    /*
+void TestGrayImage() {
     Image *img1 = new GrayImage();
     img1->LoadImage("Image-Folder/mnist/img_100.jpg");
     img1->DumpImage("img1.jpg");
-    if(ENABLE_X_SERVER){
+    if(ENABLE_X_SERVER) {
         img1->Display_X_Server();
     }
     img1->Display_ASCII();
     img1->Display_CMD();
 
-    //test bit filed
-    if (loadCase(GRAY_Sobel | GRAY_Linear , img1)){
+    // Test bit field
+    if (loadCase(option1, img1)) {
         img1->DumpImage("imgtest1.jpg");
-        if(ENABLE_X_SERVER){
+        if(ENABLE_X_SERVER) {
             img1->Display_X_Server();
         }
         img1->Display_ASCII();
         img1->Display_CMD();
     }
+    delete img1;
+}
 
+void TestRGBImage() {
     Image *img2 = new RGBImage();
     img2->LoadImage("Image-Folder/lena.jpg");
     img2->DumpImage("img2.jpg");
-    if(ENABLE_X_SERVER){
+    if(ENABLE_X_SERVER) {
         img2->Display_X_Server();
     }
-    //img2->Display_ASCII();
     img2->Display_CMD();
 
-    if(loadCase(option2, img2)){
+    if(loadCase(option2, img2)) {
         img2->DumpImage("imgtest1.jpg");
-        if(ENABLE_X_SERVER){
+        if(ENABLE_X_SERVER) {
             img2->Display_X_Server();
         }
-        //img2->Display_ASCII();
         img2->Display_CMD();
     }
-    */
+    delete img2;
+}
 
-    //// some bit field filter design driven code here
-    
-    // some photo mosaic driven code here
-
+void TestPhotoMosaic() {
     PhotoMosaic photo_mosaic;
-    Image *img3 = photo_mosaic.InputImage("Image-Folder/gril_2x.png", "Image-Folder/cifar10");
+    photo_mosaic.InputImage("Image-Folder/gril_2x.png", "Image-Folder/cifar10");
+
+    RGBImage* img3 = new RGBImage();
+    img3->LoadImage("Image-Folder/mnist/img_100.jpg");
+
     img3->DumpImage("img3.jpg");
-    if(ENABLE_X_SERVER){
+    if(ENABLE_X_SERVER) {
         img3->Display_X_Server();
     }
-    //img3->Display_ASCII();
     img3->Display_CMD();
-
 
     RGBImage Mosaiced_photo = photo_mosaic.Generate_Mosaic();
     Mosaiced_photo.DumpImage("Mosaiced_Photo.jpg");
 
-    if(ENABLE_X_SERVER){
+    if(ENABLE_X_SERVER) {
         Mosaiced_photo.Display_X_Server();
     }
-    //Mosaiced_photo.Display_ASCII();
     Mosaiced_photo.Display_CMD();
-
-    /*
-    delete img1;
-    img1 = nullptr;
-    delete img2;
-    img2 = nullptr;
-    */
-    return 0;
+    delete img3;
 }
